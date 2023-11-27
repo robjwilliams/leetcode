3bb54f1a9f171a782f96fc878b73781fce92be29
Robert Williams <robert151196@gmail.com>
2023-11-27 17:02:07 -0300

    Time: 61 ms (6.29%), Space: 16.3 MB (16.26%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,4 +1,3 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        senior_passangers = [passanger for passanger in details if int(passanger[11:13]) > 60]
-        return len(senior_passangers)
+        return len([passanger for passanger in details if int(passanger[11:13]) > 60])
