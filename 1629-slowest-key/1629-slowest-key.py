bec90cb694aaf96cadcfe0ea83acf4b897bbe135
Robert Williams <robert151196@gmail.com>
2023-12-01 18:11:03 -0300

    Time: 66 ms (28.89%), Space: 16.5 MB (57.17%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.py
@@ -0,0 +1,15 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        ans = [0, releaseTimes[0]]
+        for i in range(0, len(releaseTimes)):
+            if i == 0:
+                duration = releaseTimes[i]
+            else:
+                duration = releaseTimes[i] - releaseTimes[i - 1]
+            
+            if duration == ans[1] and keysPressed[i] > keysPressed[ans[0]]:
+                ans = [i, duration]
+            elif duration > ans[1]:
+                ans = [i, duration]
+            
+        return keysPressed[ans[0]]
