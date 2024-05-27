de50c4c5b0533481a98cf030f78f68f3005de70f
Robert Williams <robert151196@gmail.com>
2024-05-26 21:51:09 -0300

    Time: 128 ms (50.36%), Space: 18 MB (77.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,26 +5,17 @@ class Solution:
         """
         i = 0
         j = 1
-        
-        while i < len(nums) - 1:
-            # Move pointer i to the next non-zero element
-            while i < len(nums) - 1 and nums[i] != 0:
+        while i < len(nums) and j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-
-            # Move pointer j to the next non-zero element after i
-            j = max(j, i + 1)
-            while j < len(nums) - 1 and nums[j] == 0:
                 j += 1
-
-            # If nums[i] is zero and nums[j] is non-zero, swap them
-            if i < len(nums) and j < len(nums):
-                if nums[i] == 0 and nums[j] != 0:
-                    nums[i], nums[j] = nums[j], nums[i]
-
-            i += 1
-            j += 1
-            
-            
-            
-            
-        
+            elif nums[i] != 0 and nums[j] != 0:
+                i += 1
+                j += 1
+            elif nums[i] == 0 and nums[j] == 0:
+                j += 1
+            else:
+                i += 1
+        return nums
+    
