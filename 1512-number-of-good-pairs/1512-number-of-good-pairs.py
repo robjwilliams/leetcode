267750b49960d2a22b301ff11289cf553885fed7
Robert Williams <robert151196@gmail.com>
2024-06-21 15:27:35 -0300

    Time: 31 ms (88.27%), Space: 16.5 MB (26.27%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -3,7 +3,7 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         counter = Counter(nums)
         ans = 0
-        print(counter)
+
         for n in counter.values():
             ans += n * (n-1) // 2
         
