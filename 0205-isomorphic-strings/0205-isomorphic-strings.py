f1ef8389f7f71db25bbafc967564053e1a9b3013
Robert Williams <robert151196@gmail.com>
2024-06-21 15:13:25 -0300

    Time: 35 ms (90.30%), Space: 16.7 MB (82.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,23 +1,20 @@
 from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        n = len(s)
         dict1 = defaultdict(str)
         dict2 = defaultdict(str)
         
-        for i in range(len(s)):
+        for i in range(n):
             if s[i] not in dict1:
                 dict1[s[i]] = t[i]
             if t[i] not in dict2:
                 dict2[t[i]] = s[i]
-        print(dict1)
-        print(dict2)
-        for i in range(len(s)):
-            print(dict1[s[i]], t[i], '--', dict2[t[i]], s[i])
             if dict1[s[i]] != t[i] or dict2[t[i]] != s[i]:
                 return False
-            # print(s[i] in dict2, t[i] in dict1)
-        # for i in range(len(s)):
-        #     print(t[i], dict2[t[i]])
+        # for i in range(n):
+        #     if dict1[s[i]] != t[i] or dict2[t[i]] != s[i]:
+        #         return False
         
         return True
         
